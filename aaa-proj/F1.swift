954193b6b5340ff9964a3ab54247315a0e38fc9a
Brian Dang <tech@Techs-iMac-120.local>
2020-02-06 14:35:10 -0500

    1st commit

--- aaa-proj/F1.swift
+++ aaa-proj/F1.swift
@@ -8,3 +8,29 @@
 
 import Foundation
 
+class Pet {
+    private var name : String;
+    private var age : Int;
+    private var species : String;
+    
+    init (name : String, age : Int, species : String){
+        self.name = name;
+        self.age = age;
+        self.species = species;
+    }
+    func speak() -> String{
+        if (self.species == "Dog"){
+            return "Woof";
+        }
+        if (self.species == "Cat"){
+            return "Meow";
+        }
+        if (self.species == "Hampster"){
+            return "Squeek";
+        }
+        if (self.species == "Fish"){
+            return "Blub";
+        }
+        return "";
+    }
+}
---
+++ aaa-proj/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  aaa-proj
+//
+//  Created by Tech on 2020-02-06.
+//  Copyright © 2020 Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    let p1 = Pet(name : "Fluffy", age : 4, species : "Dog")
+    let p2 = Pet(name : "Moggy", age : 6, species : "Cat")
+    let p3 = Pet(name : "Goldie", age : 1, species : "Fish")
+    let p4 = Pet(name : "Snuffles", age : 2, species : "Hampster")
+}
+
